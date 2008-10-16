f01455a9dcc6dd770d2109bd1d2eafcc8e381375
Shane Hanna <shane.hanna@gmail.com>
2008-10-16 16:58:55 +1100

    Removed trailing whitespace from BigMoney.

--- lib/big_money/big_money.rb
+++ lib/big_money/big_money.rb
@@ -2,50 +2,50 @@ require 'bigdecimal'
 
 class BigMoney
   VERSION = '0.1.1'
-  
+
   class MoneyError < StandardError ; end
   class UncomparableCurrency < MoneyError ; end
-  
+
   @@default_currency = :USD
   def self.default_currency ; @@default_currency ; end
   def self.default_currency=(c) ; @@default_currency = c ; end
   def default_currency ; self.class.default_currency ; end
   def default_currency=(c) ; self.class.default_currency = c ; end
-  
+
   attr_reader :amount, :currency
-  
+
   def initialize(amount, currency = nil)
     @amount = amount.class == BigDecimal ? amount : BigDecimal.new(amount.to_s)
     @currency = currency || self.default_currency
   end
-  
+
   def eql?(other_money)
     currency == other_money.currency && amount == other_money.amount
   end
-  
+
   def ==(other_money)
     eql?(other_money)
   end
-  
+
   def <=>(other_money)
     raise UncomparableCurrency, "Cannot compare #{currency} to #{other_money.currency}" \
       unless currency == other_money.currency
-    
+
     amount <=> other_money.amount
   end
-  
+
   def -@
     BigMoney.new(-amount, currency)
   end
-  
+
   def +(val)
     op(:+, val)
   end
-  
+
   def -(val)
     op(:-, val)
   end
-  
+
   def *(val)
     op(:*, val)
   end
@@ -53,19 +53,19 @@ class BigMoney
   def /(val)
     op(:/, val)
   end
-  
+
   def to_s
     to_formatted_s('%.2f')
   end
-  
+
   def to_formatted_s(format)
     format.sub(/%s/, currency.to_s.upcase) % amount
   end
-  
+
   def to_i
     amount.to_i
   end
-  
+
   def to_f
     amount.to_f
   end
